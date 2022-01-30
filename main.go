651667e40db7966f290e75dae8b08dc34b00503b
maronghe <maronghe@didiglobal.com>
2022-01-30 21:46:24 +0800

    first commit

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello Zhongger ! This is your first Gin project: go-awesome-admin",
+			"简介": "：面前的这位Girl，你是我的最爱~ biu ~ biu~ biu~ ",
 		})
 	})
 	r.Run()
